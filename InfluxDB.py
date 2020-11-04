ff2cbedeca0fcc3d1355afb804555666e1df5b9f
flopp999 <21694965+flopp999@users.noreply.github.com>
2020-11-04 23:58:34 +0100

    Update InfluxDB.py

--- InfluxDB.py
+++ InfluxDB.py
@@ -21,19 +21,19 @@ headers = {
     'Content-Type': 'application/json',
 }
 
-data = '{ "query": "{viewer {homes {currentSubscription {priceInfo {today {total startsAt },tomorrow {total startsAt }}}}}}" }' # asking for today's hourly prices
+data = '{ "query": "{viewer {homes {currentSubscription {priceInfo {today {total startsAt },tomorrow {total startsAt }}}}}}" }' # asking for today's and tomorrow's hourly prices
 
 response = requests.post('https://api.tibber.com/v1-beta/gql', headers=headers, data=data) # make the query to Tibber
 response = response._content # selecting the important data from the response
 parsed = json.loads(response) # parse it so we can use it easier
 file = open("input.txt", "w") # create a file to store the data
 file.writelines(["# DML","\n# CONTEXT-DATABASE: "+database+"\n\n"]) # write some important lines into the file
-for data in parsed["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"]: # go through each hour
+for data in parsed["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["today"]: # go through today's hours
   time = data["startsAt"] # store the datetime
   utctime = str(datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S%z").timestamp())[:-2] # change datetime to epoch(seconds) and without decimals  
   total = str(round(data["total"]*100,2)) # recalc to kronor instead of öre
   file.writelines([measurement," ",field,"=",total," ",utctime,"\n"]) # write price and datetime to file
-for data in parsed["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["tomorrow"]: # go through each hour
+for data in parsed["data"]["viewer"]["homes"][0]["currentSubscription"]["priceInfo"]["tomorrow"]: # go through tomorrow's hours
   time = data["startsAt"] # store the datetime
   utctime = str(datetime.datetime.strptime(time, "%Y-%m-%dT%H:%M:%S%z").timestamp())[:-2] # change datetime to epoch(seconds) and without decimals
   total = str(round(data["total"]*100,2)) # recalc to kronor instead of öre
