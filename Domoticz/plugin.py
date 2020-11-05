1c12a38a025916cc794d7d675a50b42e85652998
flopp999 <21694965+flopp999@users.noreply.github.com>
2020-11-05 10:22:13 +0100

    Create plugin.py

---
+++ Domoticz/plugin.py
@@ -0,0 +1,33 @@
+# Basic Python Plugin Example
+#
+# Author: GizMoCuz
+#
+"""
+<plugin key="Tibber" name="Tibber API" author="flopp" version="1.0.0" wikilink="http://www.domoticz.com/wiki/plugins/plugin.html" externallink="https://www.google.com/">
+    <description>
+        <h2>Plugin Title</h2><br/>
+        Overview...
+        <h3>Features</h3>
+        <ul style="list-style-type:square">
+            <li>Feature one...</li>
+            <li>Feature two...</li>
+        </ul>
+        <h3>Devices</h3>
+        <ul style="list-style-type:square">
+            <li>Device Type - What it does...</li>
+        </ul>
+        <h3>Configuration</h3>
+        Configuration options...
+    </description>
+    <params>
+        <param field="Mode3" label="Tibber token" width="200px" required="true"/>
+        <param field="Mode6" label="Debug" width="100px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal" default="true" />
+                <option label="Logging" value="File"/>
+            </options>
+        </param>
+    </params>
+</plugin>
+"""
